177e36deb5ad6756c0d3f1019b0cf2eb777d106b
kilim <s28346@pjwstk.edu.pl>
2024-03-21 22:54:49 +0100

    hi

--- Task_3/Task_3/Program.cs
+++ Task_3/Task_3/Program.cs
@@ -1,8 +1,26 @@
-﻿
+﻿using Task_3;
 
-using System.ComponentModel;
+var liquidContainer = new LiquidContainer(500);
+var refrigeratedContainer = new RefrigeratedContainer(300, 5); 
+var gasContainer = new GasContainer(800, 100); 
 
-Console.WriteLine("Hello, World!");
-var container=new Container();
-List<int> list=new List<int>(){1,2,3};
-Dictionary<String,int> dictionary=new Dictionary<string, int>();
+
+var ship = new ContainerShip();
+
+
+ship.LoadContainer(liquidContainer);
+ship.LoadContainer(refrigeratedContainer);
+
+try
+{
+    gasContainer.Load(1500); 
+}
+catch (OverfillException ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
+
+ship.LoadContainer(gasContainer);
+        
+Console.WriteLine("Containers have been loaded onto the ship.");
+ship.ShowContainers();
